9bfbf8d6ffed69f04dd32e53c851589211a1ff79
Diones <pereiradiones987@gmail.com>
2025-02-07 10:51:30 -0300

    instalando e configurando validação de body

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
   Logger.log(`Servidor rodando em http://localhost:${process.env.PORT ?? 3000}`);
 }
