e4aae924418e044a9998272310eb67b4b203e1a6
Diones <pereiradiones987@gmail.com>
2025-02-07 14:50:38 -0300

    salvando usuário no banco de dados

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,28 +1,33 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
+
+  constructor(private readonly prisma: PrismaService) {}
+
   private readonly looger = new Logger(UserService.name);
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto) {
+    this.looger.log("Criando usuário");
+    return this.prisma.user.create({ data: createUserDto });
   }
 
-  findAll() {
+   async findAll() {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} user`;
   }
 }
